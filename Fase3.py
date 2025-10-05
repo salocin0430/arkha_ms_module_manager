970891ccc78af059e52a0c5db6e60c77b9ecfdff
Fcamarasa <fcamarasa13@gmail.com>
2025-10-05 15:19:30 +0200

    3.8 circ

--- Fase3.py
+++ Fase3.py
@@ -7,8 +7,8 @@ import math
 constantes = {
     "altura_modulo": 3.1,
     "ancho_modelo": 3.5,
-    "ancho_centro": 3.6,
-    "ancho_circ": 3.6,
+    "ancho_centro": 3.5,
+    "ancho_circ": 3.8,
     "x": "horizontal",
     "y": "vertical",
     "z": "profundidad",
