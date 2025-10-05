33817ff760df11d6264a6bddccf43d80bff75266
Fcamarasa <fcamarasa13@gmail.com>
2025-10-05 15:17:14 +0200

    test

--- Fase3.py
+++ Fase3.py
@@ -7,8 +7,8 @@ import math
 constantes = {
     "altura_modulo": 3.1,
     "ancho_modelo": 3.5,
-    "ancho_centro": 2.6,
-    "ancho_circ": 4,
+    "ancho_centro": 3.6,
+    "ancho_circ": 3.6,
     "x": "horizontal",
     "y": "vertical",
     "z": "profundidad",
